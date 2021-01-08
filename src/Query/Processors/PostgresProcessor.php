0ae11cd1d798186e74327815db3296f3ea04ea27
Chad Woskoski <chadw@cvtrust.org>
2021-01-08 11:54:08 -0800

    Fixed Grammer and processor.

--- src/Query/Processors/PostgresProcessor.php
+++ src/Query/Processors/PostgresProcessor.php
@@ -13,9 +13,34 @@ declare(strict_types=1);
 namespace Hyperf\Database\Postgres\Query\Processors;
 
 use Hyperf\Database\Query\Processors\Processor;
+use Hyperf\Database\Query\Builder;
 
 class PostgresProcessor extends Processor
 {
+    /**
+     * Process an "insert get ID" query.
+     *
+     * @param  Builder  $query
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string|null  $sequence
+     * @return int
+     */
+    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+    {
+        $connection = $query->getConnection();
+
+        $connection->recordsHaveBeenModified();
+
+        $result = $connection->selectFromWriteConnection($sql, $values)[0];
+
+        $sequence = $sequence ?: 'id';
+
+        $id = is_object($result) ? $result->{$sequence} : $result[$sequence];
+
+        return is_numeric($id) ? (int) $id : $id;
+    }
+
     /**
      * Process the results of a column listing query.
      *
